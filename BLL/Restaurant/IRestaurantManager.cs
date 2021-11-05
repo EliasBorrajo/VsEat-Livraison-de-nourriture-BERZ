9e4c92dc59b66a50027842111c5f049f281f90e0
renna.zacharie <renna.zacharie@gmail.com>
2021-11-05 15:57:33 +0100

    BLL documentée

--- BLL/Restaurant/IRestaurantManager.cs
+++ BLL/Restaurant/IRestaurantManager.cs
@@ -2,8 +2,15 @@
 
 namespace BLL
 {
+    /// <summary>
+    /// Interface permettant de gérer tout ce qui concerne les restaurants.
+    /// </summary>
     public interface IRestaurantManager
     {
+        /// <summary>
+        /// Méthode permettant de récupérer tous les restaurants.
+        /// </summary>
+        /// <returns>Tableau de Restaurant contenant tous les restaurants.</returns>
         Restaurant[] GetRestaurants();
     }
 }
--- BLL/Restaurant/RestaurantManager.cs
+++ BLL/Restaurant/RestaurantManager.cs
@@ -4,10 +4,20 @@ using Microsoft.Extensions.Configuration;
 
 namespace BLL
 {
+    /// <summary>
+    /// Classe qui implémente l'interface IRestaurantManager, permettant la gestion de tout ce qui concerne les restaurants.
+    /// </summary>
     public class RestaurantManager : IRestaurantManager
     {
+        /// <summary>
+        /// Objet permettant d'interagir avec la table Restaurant.
+        /// </summary>
         private IRestaurantDB RestaurantDB { get; }
 
+        /// <summary>
+        /// Constructeur pour créer un objet RestaurantManager.
+        /// </summary>
+        /// <param name="Configuration">Objet de configuration contenant la chaîne de connexion à la DB.</param>
         public RestaurantManager(IConfiguration Configuration)
         {
             RestaurantDB = new RestaurantDB(Configuration);
