5f63c5a6ac8bf8f359e696656bb2766e85357628
renna.zacharie <renna.zacharie@gmail.com>
2021-11-05 14:10:45 +0100

    DTO documentée

--- DTO/Commande.cs
+++ DTO/Commande.cs
@@ -2,18 +2,57 @@
 
 namespace DTO
 {
+    /// <summary>
+    /// Classe destinée à stocker les enregistrements de la table Commande.
+    /// </summary>
     public class Commande : IDBTable
     {
         public int ID { get; }
+        /// <summary>
+        /// Staff en charge de livrer la commande.
+        /// </summary>
         public Staff Staff { get; set; }
+        /// <summary>
+        /// Client qui a passé la commande.
+        /// </summary>
         public Client Client { get; set; }
+        /// <summary>
+        /// Plats contenus dans la commande, liés à leur quantité respective.
+        /// </summary>
         public CommandePlat[] Plats { get; set; }
+        /// <summary>
+        /// Heure à laquelle la commande a été passée.
+        /// </summary>
         public DateTime Heure { get; set; }
+        /// <summary>
+        /// Heure à laquelle le client a demandé la livraison.
+        /// </summary>
         public DateTime HeureLivraison { get; set; }
+        /// <summary>
+        /// Heure à laquelle le staff a livré la commande et encaissé le paiement.
+        /// </summary>
         public DateTime HeurePaiement { get; set; }
+        /// <summary>
+        /// Prix total de la commande.
+        /// </summary>
         public double Somme { get; set; }
+        /// <summary>
+        /// Indique si la commande a été annulée.
+        /// </summary>
         public bool Annule { get; set; }
 
+        /// <summary>
+        /// Constructeur pour créer un objet Commande.
+        /// </summary>
+        /// <param name="ID">Identifiant unique de la commande.</param>
+        /// <param name="Staff">Staff en charge de la commande.</param>
+        /// <param name="Client">Client qui a passé la commande.</param>
+        /// <param name="Plats">Plats contenus dans la commande, avec la quantité.</param>
+        /// <param name="Heure">Heure à laquelle la commande a été passée.</param>
+        /// <param name="HeureLivraison">Heure à laquelle la livraison à été demandée.</param>
+        /// <param name="HeurePaiement">Heure à laquelle le staff a livré la commande et encaissé le paiement.</param>
+        /// <param name="Somme">Prix total de la commande.</param>
+        /// <param name="Annule">Indique si la commande a été annulée.</param>
         public Commande(int ID, Staff Staff, Client Client, CommandePlat[] Plats, DateTime Heure, DateTime HeureLivraison, DateTime HeurePaiement, double Somme, bool Annule)
         {
             this.ID = ID;
--- DTO/Localite.cs
+++ DTO/Localite.cs
@@ -1,11 +1,26 @@
 ﻿namespace DTO
 {
+    /// <summary>
+    /// Classe destinée à stocker les enregistrements de la table Localite.
+    /// </summary>
     public class Localite : IDBTable
     {
         public int ID { get; }
+        /// <summary>
+        /// Nom de la localité.
+        /// </summary>
         public string Nom { get; set; }
+        /// <summary>
+        /// NPA de la localité.
+        /// </summary>
         public string NPA { get; set; }
 
+        /// <summary>
+        /// Constructeur pour créer un objet Localite.
+        /// </summary>
+        /// <param name="ID">Identifiant unique de la localité.</param>
+        /// <param name="Nom">Nom de la localité.</param>
+        /// <param name="NPA">NPA de la localité.</param>
         public Localite(int ID, string Nom, string NPA)
         {
             this.ID = ID;
