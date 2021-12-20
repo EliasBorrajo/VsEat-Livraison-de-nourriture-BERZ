0970b3d9afdf9060e91de4bded29d12c5921ee89
EliasKelliwich <borrajo.elias@gmail.com>
2021-12-20 23:02:28 +0100

    Fini l'affichage des restaurants au propre !! :)

--- VSEatWebApp/wwwroot/js/Restaurants.js
+++ VSEatWebApp/wwwroot/js/Restaurants.js
@@ -1,5 +1,16 @@
-﻿$(".option").click(function ()
+﻿// symbole " $ ", permet de récuperer un élément du html
+
+$(".option").hover(function ()
 {
     $(".option").removeClass("active");
     $(this).addClass("active");
 });
+
+// Sur le click de l'image, redirige pour lancer la commande ! :) 
+// On est heureux avec Zack pour ce projet BEEEERZ !
+function RedirectToDetails(id)
+{
+    window.location.href = $("#AddCommande").val().replace('__id__', id);
+};
+
+
